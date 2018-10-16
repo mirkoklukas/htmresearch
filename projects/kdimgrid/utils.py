17be178b96d9fea3b980089c86c9546c8ee8aa5f
Mirko Klukas <mklukas@Mirkos-MacBook-Pro.local>
2018-10-16 11:43:33 -0700

    add functions to create action tensors

--- projects/kdimgrid/utils.py
+++ projects/kdimgrid/utils.py
@@ -2,8 +2,6 @@ import numpy as np
 
 
 
-
-
 def create_block_A(m, k, S):
 
 
@@ -18,5 +16,38 @@ def create_block_A(m, k, S):
 
             A[i,:,i_] = a / s
 
+
+    return A
+
+def create_random_A_normal(m, k):
+
+    A = np.random.standard_normal(size=(m,2,k))
+
+    return A
+
+def create_random_A_shuffled(m, k, S):
+
+    A = np.zeros((m*2,k))
+    for l in range(k):
+      v = np.zeros((m,2))
+      for i in range(m):
+        a  = np.random.randn(2)
+        a /= np.linalg.norm(a)
+        v[i,:] = a 
+      S_ = S[np.random.permutation(m)].reshape((m,1))
+
+      A[:,l] = (v/S_).reshape(2*m)
+
+    return A.reshape((m,2,k))
+
+
+def create_random_A(m, k, S):
+
+    A = np.zeros((m,2,k))
+    for i, s in enumerate(S):
+        for l in xrange(k):
+            a  = np.random.randn(2)
+            a /= np.linalg.norm(a)
+            A[i,:,l] = a / s
 
     return A
